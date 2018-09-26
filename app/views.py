73621a9abcfc29f1e0e1e4060707dce379304d7a
Dave Fuller <dave@accentdesign.co.uk>
2018-09-26 17:15:30 +0100

    Addition of error pages

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.template import loader
 from django.views.generic import TemplateView
 
 
 class Home(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
+
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    context = {'request': request}
+    return HttpResponse(content=template.render(context), content_type='text/html; charset=utf-8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+    context = {'request': request}
+    return HttpResponse(content=template.render(context), content_type='text/html; charset=utf-8', status=500)
