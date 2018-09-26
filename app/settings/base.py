bc79149371f40f3ea5579da8b178d1bff2afb4fa
Stuart George <stuart@accentdesign.co.uk>
2018-09-26 09:06:04 +0100

    aws qs auth

--- app/settings/base.py
+++ app/settings/base.py
@@ -157,6 +157,7 @@ AWS_S3_CUSTOM_DOMAIN = None  # Add for cloudfront etc
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_QUERYSTRING_AUTH = False
 AWS_QUERYSTRING_EXPIRE = 3600
 AWS_S3_FILE_OVERWRITE = False
 AWS_IS_GZIPPED = True
