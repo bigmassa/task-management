2b19e620e46bb7b8e6be2105cdddd33490d68d4a
Stuart George <stuart@accentdesign.co.uk>
2018-10-03 09:33:01 +0100

    cleaner client list build and tweaks

--- static/dist/js/cloc.js
+++ static/dist/js/cloc.js
@@ -1,5 +1,27 @@
 $(function() {
 
+    function readableTextColor(color) {
+        var brightStart = 123;
+        function hexToRgb(hex) {
+            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+            hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+                return r + r + g + g + b + b;
+            });
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            } : null;
+        }
+        var colourIsLight = function (r, g, b) {
+            var a = (r * 299 + g * 587 + b * 114) / 1000;
+            return (a > brightStart);
+        };
+        var bgRgb = hexToRgb(color);
+        return colourIsLight(bgRgb.r, bgRgb.g, bgRgb.b) ? 'black' : 'white';
+    }
+
     function getClients() {
         return $.ajax({
             type: "GET",
@@ -27,57 +49,60 @@ $(function() {
 
     function buildClientList(show) {
         $.when(getClients(), getJobs(), getTasks()).done(function (clients, jobs, tasks) {
-            // loop each task and dynamically add it to the list of tasks under the client's jobs
+            // find all client > jobs > tasks that have been searched for and construct the tree
             var clientTree = $("<ul></ul>").addClass("client-list");
-
-            $.each(tasks[0], function(index, task) {
-                var clientNode, jobNode, taskNode, foundNodes;
-                var job = jobs[0].find(item => item.id === task.job);
-                var client = clients[0].find(item => item.id === job.client);
-
-                foundNodes = clientTree.find("[data-client-id=" + client.id + "]");
-                if (foundNodes.length == 0) {
-                    clientNode = $('<li data-client-id="' + client.id + '"></li>');
-                    var clientLink = $("<a></a>").addClass("client").text(client.name);
-                    clientNode.append(clientLink).append('<ul></ul>');
-                    clientTree.append(clientNode);
-                } else {
-                    clientNode = $(foundNodes[0]);
-                }
-
-                foundNodes = clientNode.find("[data-job-id=" + job.id + "]");
-                if (foundNodes.length == 0) {
-                    jobNode = $('<li data-job-id="' + job.id + '"></li>');
+            var jobIds = [...new Set(tasks[0].map(o => o.job))];
+            var foundJobs = jobs[0].filter(o => jobIds.includes(o.id));
+            var clientIds = [...new Set(foundJobs.map(o => o.client))];
+            var foundCLients = clients[0].filter(o => clientIds.includes(o.id));
+            // loop each client and add the node
+            $.each(foundCLients, function (index, client) {
+                var clientNode = $('<li data-client-id="' + client.id + '"></li>');
+                var clientLink = $("<a></a>").addClass("client").text(client.name);
+                clientNode.append(clientLink).append('<ul></ul>');
+                // append the node
+                clientTree.append(clientNode);
+                // filter jobs in client
+                var jobsInClient = foundJobs.filter(o => o.client === client.id);
+                // loop each job and add the node
+                $.each(jobsInClient, function (index, job) {
+                    var jobNode = $('<li data-job-id="' + job.id + '"></li>');
+                    // hide node if its passed in
                     if (!show) {
                         jobNode.hide();
                     }
                     var jobLink = $("<a></a>").addClass("job").text(job.title);
                     jobNode.append(jobLink).append('<ul></ul>');
+                    // append the node
                     clientNode.find('ul').first().append(jobNode);
-                } else {
-                    jobNode = $(foundNodes[0]);
-                }
-
-                taskNode = $('<li data-task-id="' + task.id + '" data-title="' + task.title + '" data-colour="' + job.colour + '"></li>');
-                taskNode.text(task.title).addClass("task").css("background", job.colour);
-
-                if (!show) {
-                    taskNode.hide();
-                }
-
-                taskNode.draggable({
-                    cursor: "move",
-                    zIndex: 100,
-                    revert: true,
-                    revertDuration: 0,
-                    helper: function(event) {
-                        return $( "<div class='task-drag-element'>" + task.title + "</div>" );
-                    }
+                    // filter tasks in job
+                    var tasksInJob = tasks[0].filter(o => o.job === job.id);
+                    // loop each task and add the node
+                    $.each(tasksInJob, function (index, task) {
+                        var taskNode = $('<li data-task-id="' + task.id + '" data-title="' + task.title + '" data-colour="' + job.colour + '"></li>');
+                        taskNode.text(task.title).addClass("task").css("background", job.colour).css("color", readableTextColor(job.colour));
+                        // hide node if its passed in
+                        if (!show) {
+                            taskNode.hide();
+                        }
+                        // make the task draggable for the calendar
+                        taskNode.draggable({
+                            cursor: "move",
+                            zIndex: 100,
+                            revert: true,
+                            revertDuration: 0,
+                            helper: function(event) {
+                                return $( "<div class='task-drag-element'>" + task.title + "</div>" );
+                            }
+                        });
+                        // append the node
+                        jobNode.find('ul').first().append(taskNode);
+                    });
                 });
-                jobNode.find('ul').first().append(taskNode);
+                // finally add the tree to the page
+                $(".client-list-wrapper").html(clientTree);
             });
 
-            $(".client-list-wrapper").html(clientTree);
         });
     }
 
@@ -151,7 +176,7 @@ $(function() {
                             comments: $(this).attr('comments'),
                             color: $(this).attr('colour'),
                             duration: $(this).attr('duration'),
-                            textColor: 'black'
+                            textColor: readableTextColor($(this).attr('colour'))
                         });
                     });
                     if (callback) callback(events);
@@ -211,7 +236,7 @@ $(function() {
             var currentday = moment(event.start).format("YYYY-MM-DD");
             var ctrl = $(".fc-day-header[data-date=" + currentday + "] em");
 
-            if (event.duration) {
+            if (event.duration && ctrl.text() !== "00:00") {
                 var prev = moment.duration(ctrl.text() || "00:00");
                 var thisEvent = moment.duration(event.duration);
                 var total = prev.subtract(thisEvent);
@@ -242,7 +267,7 @@ $(function() {
                         comments: $(data).attr('comments'),
                         color: $(data).attr('colour'),
                         duration: $(data).attr('duration'),
-                        textColor: 'black',
+                        textColor: readableTextColor($(data).attr('colour')),
                     };
                     $('.cloc').fullCalendar('renderEvent', event, false);
                 },
@@ -289,7 +314,7 @@ $(function() {
                             comments: $(data).attr('comments'),
                             color: $(data).attr('colour'),
                             duration: $(data).attr('duration'),
-                            textColor: 'black',
+                            textColor: readableTextColor($(data).attr('colour')),
                         };
                         $('.cloc').fullCalendar('renderEvent', event, false);
                     },
@@ -326,7 +351,7 @@ $(function() {
 
             // sets the detail in the form from the event data
             clocOverlay.find('.title').html(calEvent.title);
-            clocOverlay.css("background-color", calEvent.color);
+            clocOverlay.find('.overlay-header .color-indicator').css("background-color", calEvent.color);
             clocOverlay.find('[name=task]').val(calEvent.task);
             clocOverlay.find('[name=comments]').val(calEvent.comments);
             clocOverlay.find('[name=start_time]').val(calEvent.start.format("HH:mm"));
@@ -349,9 +374,9 @@ $(function() {
             // setup update event
             clocOverlay.find('form').on('submit', function (evt) {
                 var task = $(this).find('[name=task]').val();
+                var comments = $(this).find('[name=comments]').val();
                 var startSplit = $('[name=start_time]').val().split(':');
                 var endSplit = $('[name=end_time]').val().split(':');
-                var comments = $(this).find('[name=comments]').val();
                 $.ajax({
                     type: "PATCH",
                     url: timeEntryListUrl + calEvent.id + '/',
@@ -371,6 +396,7 @@ $(function() {
                         calEvent.comments = $(data).attr('comments');
                         calEvent.color = $(data).attr('colour');
                         calEvent.duration = $(data).attr('duration');
+                        calEvent.textColor = readableTextColor($(data).attr('colour'));
 
                         $('.cloc').fullCalendar('updateEvent', calEvent);
 
@@ -409,7 +435,7 @@ $(function() {
 
                 clocOverlay.find('[name=task]').val(task);
                 clocOverlay.find('.title').html(title);
-                clocOverlay.css("background-color", colour);
+                clocOverlay.find('.overlay-header .color-indicator').css("background-color", colour);
 
                 evt.stopPropagation();
             });
