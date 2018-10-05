87bd41e6668b56eab98d266d14ee6016290d879a
Dave Fuller <dave@accentdesign.co.uk>
2018-10-05 14:45:59 +0100

    Addition of active state for navigation items

--- wip/templatetags/wip_tags.py
+++ wip/templatetags/wip_tags.py
@@ -2,3 +2,19 @@ from django import template
 
 
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def section_name(context):
+    """ Finds the section name for a given url """
+
+    url_name = context['request'].resolver_match.url_name
+
+    if url_name.startswith("task-board"):
+        return 'taskboard'
+
+    if url_name.startswith("timesheet"):
+        return 'timesheet'
+
+    if url_name.startswith(("client", "job", "task")):
+        return 'client'
