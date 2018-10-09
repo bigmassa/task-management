3744ca4e46e9d44c92e2ff16764dfadaad8c3ac0
Stuart George <stuart@accentdesign.co.uk>
2018-10-09 14:06:03 +0100

    sort task schema

--- wip/api/job.py
+++ wip/api/job.py
@@ -1,8 +1,11 @@
+import coreapi
+import coreschema
 from django_filters import FilterSet
 from django_filters import rest_framework as filters
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.schemas import ManualSchema
 
 from wip.models import Job, Task
 from wip.serializers import JobSerializer, JobTaskSortSerializer
@@ -12,15 +15,39 @@ class JobFilter(FilterSet):
     for_timesheet = filters.BooleanFilter(field_name='status__allow_new_timesheet_entries')
 
 
+sort_schema = ManualSchema(
+    fields=[
+        coreapi.Field(
+            "id",
+            required=True,
+            location="path",
+            schema=coreschema.String(
+                title="ID",
+                description="Job ID",
+            )
+        ),
+        coreapi.Field(
+            "tasks",
+            required=True,
+            location='form',
+            schema=coreschema.Array(
+                title="Task ID's",
+                description="List of Task ID's",
+                items=coreschema.Integer()
+            )
+        ),
+    ],
+    description="Sort tasks for a job.",
+)
+
+
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     filter_class = JobFilter
 
-    @action(detail=True, methods=['post'], url_path='sort-tasks')
+    @action(detail=True, methods=['post'], url_path='sort-tasks', schema=sort_schema)
     def sort_tasks(self, request, pk=None):
-        """ Seperate endpoint to sort tasks without posting an entire data set """
-
         serializer = JobTaskSortSerializer(data=request.data)
         if serializer.is_valid():
             for index, pk in enumerate(serializer.data['tasks']):
