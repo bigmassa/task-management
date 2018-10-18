506fef4687fc8d3507b922e51805c7f6a31759bb
Stuart George <stuart.bigmassa@gmail.com>
2018-10-18 12:17:05 +0100

    adding the schema drf 3.9

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
+from rest_framework.renderers import JSONOpenAPIRenderer
 from rest_framework.schemas import get_schema_view
 
 from app import routers
@@ -17,7 +18,10 @@ from wip.urls import router as wip_router
 API_TITLE = 'WIP API'
 
 # api schemas
-schema_view = get_schema_view(title=API_TITLE)
+schema_view = get_schema_view(
+    title=API_TITLE,
+    renderer_classes=[JSONOpenAPIRenderer]
+)
 
 # api router
 router = routers.DefaultRouter()
@@ -28,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include((router.urls, 'api'))),
     path('api/docs/', include_docs_urls(title=API_TITLE)),
+    path('api/schema.json', schema_view),
     path('auth/', include('authentication.urls')),
     path('reporting/', include('reporting.urls')),
     path('wip/', include('wip.urls')),
