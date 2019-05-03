7cc519304ff6e2d5925e3a7a721470540e60a881
Adam Halesworth <a.halesworth@gmail.com>
2019-05-03 17:31:48 +0100

    Added scheduled task to remind users to fill in their timesheet

--- app/tasks.py
+++ app/tasks.py
@@ -1,53 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.core.mail import EmailMessage
 from django.db import models
-
 from huey import crontab
-from huey.contrib.djhuey import db_task, task
+from huey.contrib.djhuey import db_task, db_periodic_task, task
 import requests
 
 from common.models import WIPSettings
+from authentication.models import User
 
 
 HOST = 'https://wip.accentdesign.co.uk'
-
-
-class SlackNotification:
-    def __init__(self, task):
-        self.task = task
-
-    def json(self):
-        client_name = self.task.job.client.name
-        job_title = self.task.job.title
-
-        client_link = f'{HOST}/clients/{self.task.job.client_id}'
-        job_link = f'{HOST}/clients/{self.task.job.client_id}/jobs/{self.task.job_id}'
-        title_link = f'{job_link}?task={self.task.id}'
-        footer = f'<{client_link}|{client_name}> - <{job_link}|{job_title}>'
-
-        return {
-            "channel": self.task.job.slack_channel_id,
-            "attachments": [
-                {
-                    "fallback": f'Task updated: {self.task.title}',
-                    "color": self.task.job.client.colour,
-                    "pretext": "Task updated",
-                    "title": self.task.title,
-                    "title_link": title_link,
-                    "text": self.task.description,
-                    "fields": [
-                        {
-                            "title": "Status",
-                            "value": str(self.task.status),
-                            "short": False
-                        }
-                    ],
-                    "footer": footer,
-                    "ts": datetime.now().timestamp()
-                }
-            ]
-        }
 
 
 @db_task()
@@ -66,7 +29,7 @@ def notify_status_change(task, to=None):
         return
 
     if task.job.slack_channel_id:
-        send_slack_notification(task)
+        send_task_slack_notification(task)
 
     send_email(task, to, None, notify_job_relationships, notify_task_assignees)
 
@@ -99,12 +62,122 @@ def send_email(task, to, reply_to, notify_relationships, notify_assignees):
 
 
 @task(retries=3, retry_delay=60)
-def send_slack_notification(task):
+def send_task_slack_notification(task):
     """ Send a Slack notification. """
 
+    client_name = task.job.client.name
+    job_title = task.job.title
+    client_link = f'{HOST}/clients/{task.job.client_id}'
+    job_link = f'{HOST}/clients/{task.job.client_id}/jobs/{task.job_id}'
+    title_link = f'{job_link}?task={task.id}'
+    footer = f'<{client_link}|{client_name}> - <{job_link}|{job_title}>'
+
+    post_slack_message({
+        "channel": task.job.slack_channel_id,
+        "attachments": [
+            {
+                "fallback": f'Task updated: {task.title}',
+                "color": task.job.client.colour,
+                "pretext": 'Task updated',
+                "title": task.title,
+                "title_link": title_link,
+                "text": task.description,
+                "fields": [
+                    {
+                        "title": "Status",
+                        "value": str(task.status),
+                        "short": False
+                    }
+                ],
+                "footer": footer,
+                "ts": datetime.now().timestamp()
+            }
+        ]
+    })
+
+
+@db_periodic_task(crontab(day_of_week='1,2,3,4,5', hour='9-18'))
+def check_timesheets():
+    """ Check timesheet entries for all active users. """
+    from wip.models import TimeEntry
     settings = WIPSettings.load()
 
+    working_week_days = 5
+    today = datetime.today()
+    yesterday = today - timedelta(days=1)
+    cutoff = today - timedelta(days=settings.timesheet_check_range)
+
+    eligible_users_to_remind = User.objects.filter(
+        is_active=True,
+        slack_id__isnull=False,
+        requires_timesheet_reminders=True
+    )
+
+    for user in eligible_users_to_remind:
+        channel = user.slack_id
+        if not channel:
+            continue
+
+        incomplete_time_entries = (
+            TimeEntry.objects
+            .filter(
+                ended_at__date__range=(cutoff, yesterday),
+                signed_off=False
+            )
+            .values('id')[:1]
+        )
+
+        # If there's a single timesheet entry not signed off
+        # just warn the user.
+        if incomplete_time_entries:
+            timesheet_reminder(
+                user.slack_id, user.get_full_name(), settings.timesheet_check_range
+            )
+            return
+
+        # If the user has signed off timesheet entries, check
+        # if there are 5 days' worth of them.
+        completed_time_entries = (
+            TimeEntry.objects
+            .values('ended_at__date')
+            .filter(
+                ended_at__date__range=(cutoff, yesterday),
+                signed_off=True
+            )
+            .order_by('ended_at__date')
+        ).distinct()
+
+        finished_days = len(completed_time_entries)
+        if finished_days >= working_week_days:
+            return
+
+        timesheet_reminder(
+            user.slack_id, user.get_full_name(), settings.timesheet_check_range
+        )
+
+
+def timesheet_reminder(channel, name, check_range):
+    post_slack_message(
+        {
+            "channel": channel,
+            "attachments": [
+                {
+                    "fallback": "Timesheet Reminder",
+                    "color": "#ff4757",
+                    "pretext": "Timesheet Reminder",
+                    "title": "Your timesheet is incomplete",
+                    "text": f"You haven't signed off all your timesheet entries for the past {check_range} days.",
+                    "footer": f"<http://wip.accentdesign.co.uk/timesheets|WIP Timesheet - {name}> ",
+                    "ts": datetime.now().timestamp()
+                }
+            ]
+        }
+    )
+
+
+def post_slack_message(payload):
+    settings = WIPSettings.load()
     requests.post(
         f'https://slack.com/api/chat.postMessage',
-        json=SlackNotification(task).json(),
+        json=payload,
         headers={'Authorization': f'Bearer {settings.slack_authentication_token}'})
