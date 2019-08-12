eb2557a2d5fb5c125ec0e88fce4691753eb1fe94
Adam Halesworth <a.halesworth@gmail.com>
2019-08-12 11:48:40 +0100

    Board order migration now copies existing ordering from the task it refers to as defaults

--- wip/migrations/0033_taskassignee_board_order.py
+++ wip/migrations/0033_taskassignee_board_order.py
@@ -3,6 +3,16 @@
 from django.db import migrations, models
 
 
+def set_default_assignee_order(apps, schema_editor):
+    TaskAssignee = apps.get_model('wip', 'TaskAssignee')
+
+    db_alias = schema_editor.connection.alias
+
+    for assignee in TaskAssignee.objects.using(db_alias).all():
+        assignee.board_order = assignee.task.order
+        assignee.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,4 +25,5 @@ class Migration(migrations.Migration):
             name='board_order',
             field=models.FloatField(default=16384, help_text='Task order for this assignee on the personal task board'),
         ),
+        migrations.RunPython(set_default_assignee_order)
     ]
